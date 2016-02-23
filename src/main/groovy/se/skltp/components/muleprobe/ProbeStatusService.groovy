e98311c845db59fa2740fa10a759eadd1f084c63
Håkan Dahl <hakan.dahl@gmail.com>
2016-02-23 14:37:53 +0100

    SKLTP-812: mule-probe does not respond with unavailable when probe-file is missing Step 1: refactored code to enable testing and added test to expose problem (test now fails)

--- src/main/groovy/se/skltp/components/muleprobe/ProbeStatusService.groovy
+++ src/main/groovy/se/skltp/components/muleprobe/ProbeStatusService.groovy
@@ -49,19 +49,32 @@ public class ProbeStatusService {
 			</soapenv:Envelope>
 		'''
 	
-	final RecursiveResourceBundle rrb = new RecursiveResourceBundle("mule-probe-config","mule-probe-config-override")
+	RecursiveResourceBundle rrb
+	String probeFilePath
+	String downCriteria	
+	String defaultConnectionTimeout	
+	String defaultResponseTimeout	
+	String defaultOkReturnString	
+	File probeFile
 	
-	final String probeFilePath = rrb.getString('PROBESERVICE_FILE')
+	String configOverrideFileName;
 	
-	final String downCriteria = rrb.getString('PROBE_DOWN_CRITERIA')
+	public void setConfigOverrideFileName(String configOverrideFileName) {
+		this.configOverrideFileName = configOverrideFileName
+	}
 	
-	final String defaultConnectionTimeout = rrb.getString("CONNECTION_TIMEOUT_MS")
-	
-	final String defaultResponseTimeout = rrb.getString("SO_TIMEOUT_MS")
-	
-	final String defaultOkReturnString = rrb.getString("PROBE_RETURN_OK_STRING");
-	
-	def probeFile = new File(probeFilePath)
+	// must be set up as init-method in spring-config
+	public init() {
+		rrb = new RecursiveResourceBundle("mule-probe-config",configOverrideFileName)
+
+		probeFilePath = rrb.getString('PROBESERVICE_FILE')
+		downCriteria = rrb.getString('PROBE_DOWN_CRITERIA')
+		defaultConnectionTimeout = rrb.getString("CONNECTION_TIMEOUT_MS")
+		defaultResponseTimeout = rrb.getString("SO_TIMEOUT_MS")
+		defaultOkReturnString = rrb.getString("PROBE_RETURN_OK_STRING")
+		
+		probeFile = new File(probeFilePath)	
+	}	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
